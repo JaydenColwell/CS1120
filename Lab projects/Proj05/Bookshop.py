c115a76eab9956718c2dcdfc24cc02ced224f737
Jayden Colwell <65059633+JaydenColwell@users.noreply.github.com>
2025-04-08 23:47:28 -0400

    Proj05 complete. Commented

--- Lab projects/Proj05/Bookshop.py
+++ Lab projects/Proj05/Bookshop.py
@@ -1,10 +1,15 @@
+# Project No.: 5
+# Author: Jayden Colwell
+# Description: Class bookshop which takes an orders list and manipulates it to get lots of specific data from methods
 class Bookshop:
+    # Initializes with orders list for each of the methods to use
     def __init__(self):
         self.orders = [[1, ("5464", 4, 9.99), ("8274", 18, 12.99), ("9744", 9, 44.95)],
                        [2, ("5464", 9, 9.99), ("9744", 9, 44.95)],
                        [3, ("5464", 9, 9.99), ("88112", 11, 24.99)],
                        [4, ("8732", 7, 11.99), ("7733", 11, 18.99), ("88112", 5, 39.95)]]
 
+    # reduces list to total price instead of price and quantity. Also adds $10 if less than $100 total
     def book_to_total(self):
         new_list = list()
         for order in self.orders:
@@ -17,6 +22,7 @@ class Bookshop:
             new_list.append(temp)
         return new_list
 
+    # Gets the minimum total price for each order and returns the list of each
     def remove_min(self):
         new_list = list()
         for order in self.orders:
@@ -29,6 +35,7 @@ class Bookshop:
             new_list.append((order[0], minimum_book))
         return new_list
 
+    # Gets the maximum total price for each order and returns the list of each
     def remove_max(self):
         new_list = list()
         for order in self.orders:
@@ -41,6 +48,7 @@ class Bookshop:
             new_list.append((order[0], maximum_book))
         return new_list
 
+    # Calculates total order price and returns a list of bookshop order number and total prices
     def total_cost(self):
         new_list = list()
         for order in self.orders:
@@ -51,6 +59,7 @@ class Bookshop:
             new_list.append((order[0], total))
         return new_list
 
+    # Makes a dictionary with book numbers and total ordered price and returns the biggest
     def step5(self):
         new_list = list()
         temp_dict = dict()
@@ -70,6 +79,7 @@ class Bookshop:
         new_list.append(max_price)
         return new_list
 
+    # Makes a dictionary with book numbers and total amount ordered and returns the biggest
     def step6(self):
         new_list = list()
         temp_dict = dict()
@@ -89,6 +99,7 @@ class Bookshop:
         new_list.append(max_num)
         return new_list
 
+    # Finds bookshop order numbers and total quantity and returns a list sorted by quantity per order
     def step7(self):
         new_list = list()
         for order in self.orders:
@@ -100,6 +111,7 @@ class Bookshop:
         new_list = sorted(new_list, key=lambda x: x[1], reverse=True)
         return new_list
 
+    # Returns total quantity of books ordered
     def step8(self):
         total = 0
         for order in self.orders:
@@ -107,6 +119,7 @@ class Bookshop:
                 total += book[1]
         return total
 
+    # Returns most ordered book number and least ordered book number
     def step9(self):
         new_list = list()
         temp_dict = dict()
@@ -131,6 +144,7 @@ class Bookshop:
         new_list.append(min_book)
         return new_list
 
+    # Makes a list of the length of each order and returns it
     def step10(self):
         new_list = list()
         for order in self.orders:
--- Lab projects/Proj05/TestBookshop.py
+++ Lab projects/Proj05/TestBookshop.py
@@ -1,10 +1,15 @@
+# Project No.: 5
+# Author: Jayden Colwell
+# Description: Test framework using unittest for each of the steps of Bookshop
 import unittest
 from Bookshop import Bookshop
 
 class TestBookshop(unittest.TestCase):
+    # Set up for testcase declaring a bookshop object
     def setUp(self):
         self.shop = Bookshop()
 
+    # Test for step 1 using assert equal and printing expected and actual values
     def test1(self):
         actual_val = self.shop.book_to_total()
         expected_val = [[1, ("5464", 49.96), ("8274",233.82), ("9744", 404.55)],
@@ -14,54 +19,63 @@ class TestBookshop(unittest.TestCase):
         print(f'\nTest 1: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 1 failed ###")
 
+    # Test for step 2 using assert equal and printing expected and actual values
     def test2(self):
         actual_val = self.shop.remove_min()
         expected_val = [(1,"5464"), (2,"5464"), (3,"5464"), (4,"8732")]
         print(f'\nTest 2: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 2 failed ###")
 
+    # Test for step 3 using assert not equal and printing expected and actual values
     def test3(self):
         actual_val = self.shop.remove_max()
-        expected_val = [(1,"9744"), (2,"9744"), (3,"88112"), (4,"7733")]
+        expected_val = [(1,"9745"), (2,"9744"), (3,"88112"), (4,"7733")]
         print(f'\nTest 3: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertNotEqual(actual_val, expected_val, "### test 3 succeeded ###")
 
+    # Test for step 4 using assert equal and printing expected and actual values
     def test4(self):
         actual_val = self.shop.total_cost()
         expected_val = [(1, 678.33), (2, 494.46), (3, 364.8), (4, 492.57)]
         print(f'\nTest 4: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 4 failed ###")
 
+    # Test for step 5 using assert in and printing expected and actual values
     def test5(self):
         actual_val = self.shop.step5()
         expected_val = ["9744", 809.1]
         print(f'\nTest 5: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
-        self.assertIn(expected_val, actual_val, "### test 5 failed ###")
+        self.assertIn(expected_val, [actual_val], "### test 5 failed ###")
 
+    # Test for step 6 using assert equal and printing expected and actual values
     def test6(self):
         actual_val = self.shop.step6()
         expected_val = ["5464", 22]
         print(f'\nTest 6: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 6 failed ###")
 
+    # Test for step 7 using assert equal and printing expected and actual values
     def test7(self):
         actual_val = self.shop.step7()
         expected_val = [(1, 31), (4, 23), (3, 20), (2, 18)]
         print(f'\nTest 7: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 7 failed ###")
 
+    # Test for step 8 using assert equal and printing expected and actual values
     def test8(self):
         actual_val = self.shop.step8()
         expected_val = 92
         print(f'\nTest 8: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 8 failed ###")
 
+    # Test for step 9 using assert equal and printing expected and actual values
     def test9(self):
         actual_val = self.shop.step9()
         expected_val = ["5464", "8732"]
         print(f'\nTest 9: \nExpected: \n{expected_val} \nActual: \n{actual_val}')
         self.assertEqual(actual_val, expected_val, "### test 9 failed ###")
 
+    # Test for step 10 using assert equal and printing expected and actual values
     def test10(self):
         actual_val = self.shop.step10()
         expected_val = [4, 3, 3, 4]
